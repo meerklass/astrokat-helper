a32162b0997b4e69fcdc6d1516cdc05e352d6799
amadeus <amsl@gmx.at>
2023-11-06 15:08:13 +0100

    add setup, init and gitignore

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='astrokat-helper',
+    version='0.0.1',
+    description='Helper scripts for astrokat',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[
+        'astrokat',
+    ],
+    classifiers=[
+        'GPLv3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
