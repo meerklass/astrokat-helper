b14da8d824ae8838e05107b046330bc7e522bd75
amadeus <amsl@gmx.at>
2023-11-28 14:37:40 +0100

    install when_is_patch_observable

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ setup(
     install_requires=[
         'astrokat',
     ],
+    entry_points={
+        'console_scripts': [
+            'when_is_patch_observable = scripts.when_is_patch_observable:main',
+        ]
+    },
     classifiers=[
         'GPLv3',
         'Programming Language :: Python :: 3.10',
